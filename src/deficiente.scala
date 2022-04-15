3dda45a81be56e8f04b97438267b21b170e06af2
Francisco <fxgonzalez5@utpl.edu.ec>
2022-04-14 19:41:25 -0500

    programas

---
+++ src/deficiente.scala
@@ -0,0 +1,15 @@
+object deficiente extends App {
+  // Lista de # aleatorios
+  val lista = List(241216,78954,821980,129099,43669,49942,815123,595895,956773,831554,917385,643888,979993,100628,
+    738165,192676,229071,757618,299809,103104,901994,848112,481806,125291,480476)
+
+  // Función para verificar si un número es deficiente
+  def isDeficiente (n:Int) ={
+    val suma = (1 to n-1).filter(k => n%k == 0).sum
+    if(suma < n)
+      true
+    else
+      false
+  }
+  print(lista.filter(x => isDeficiente(x)))
+}
--- src/par.scala
+++ src/par.scala
@@ -1,11 +1,9 @@
-object Ascendete extends App {
+object par extends App {
   // Lista de # aleatorios
   val lista = List(241216,78954,821980,129099,43669,49942,815123,595895,956773,831554,917385,643888,979993,100628,
     738165,192676,229071,757618,299809,103104,901994,848112,481806,125291,480476)
 
-  println("Lista ordenada ascendetemente:")
-  for (l <- lista.sorted) println(l)
-
-  println("Lista ordenada descendetemente:")
-  for (l <- lista.sorted.reverse) println(l)
+  // Función para verificar si un número es par
+  def isPar (n:Int) = (n%2 == 0)
+  print(lista.filter(x => isPar(x)))
 }
